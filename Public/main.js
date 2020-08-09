55861caf166977a40468783f3a5a37ab7070847e
Gust van de Wal <gustvandewal@gmail.com>
2020-08-09 16:43:49 +0200

    First pairprogramming changes

--- Public/main.js
+++ Public/main.js
@@ -4,47 +4,50 @@ SOCKET.emit('user-connected')
 
 SOCKET.on('user-connected', name => {
     if(name !== null){
-        alert(name + " joined")
+        alert(name + ' joined')
     }
 })
 
 SOCKET.on('user-disconnected', name => {
     if(name !== null){
-        alert(name + " left")
+        alert(name + ' left')
     }
 })
 
-SOCKET.on('init', games => {
+SOCKET.on('init', everything => {
 
-    let gameList = document.querySelector("#pick_game > ul")
-    let userList = document.querySelector("#pick_user > ul")
+    let gameList = document.querySelector('#pick_game > ul')
+    let userList = document.querySelector('#pick_user > ul')
 
     choice = {}
 
-    games.forEach(game => {
-        gameListItem = document.createElement("li")
-        gameListItem.innerHTML = game.name
-        gameListItem.addEventListener("click", e => {
+    const USERS = everything.users
+    const GAMES = everything.games
 
-            document.querySelector("#pick_game").style.display = "none"
-            choice.game = game.name
+    for (const [ name, game ] of Object.entries(GAMES)) {
+        gameListItem = document.createElement('li')
+        gameListItem.innerHTML = name
+        gameListItem.addEventListener('click', e => {
 
-            game.users.forEach(user => {
+            document.querySelector('#pick_game').style.display = 'none'
+            choice.game = name
 
-                userListItem = document.createElement("li")
-                userListItem.innerHTML = user.name
-                userListItem.addEventListener("click", e => {
+            game.users.forEach(userID => {
 
-                    document.querySelector("#pick_user").style.display = "none"
-                    choice.user = user.name
-                    SOCKET.emit("pick-game", choice)
+                userListItem = document.createElement('li')
+                const USER = USERS[userID]
+                userListItem.innerHTML = USER.name
+                userListItem.addEventListener('click', e => {
+
+                    document.querySelector('#pick_user').style.display = 'none'
+                    choice.user = userID
+                    SOCKET.emit('pick-game', choice)
                 })
                 userList.appendChild(userListItem)
             })
         })
         gameList.appendChild(gameListItem)
-    });
-
+    }
 
 })
 
--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-const EXPRESS = require("express")
-const HTTP = require('http')
-const PORT = 3000
-const SOCKETIO = require("socket.io")(PORT + 100)
-const FS = require('fs')
+const EXPRESS       = require('express')
+const HTTP          = require('http')
+const PORT          = 3000
+const SOCKETIO      = require('socket.io')(PORT + 100)
+const FS            = require('fs')
 
-const DATABASE_FILE = "./Utils/database.json"
-const APP = EXPRESS()
-const SERVER = HTTP.createServer(APP)
-const IO = SOCKETIO.listen(SERVER)
+const DATABASE_FILE = './Utils/database.json'
+const APP           = EXPRESS()
+const SERVER        = HTTP.createServer(APP)
+const IO            = SOCKETIO.listen(SERVER)
 
 
-const { Socket } = require("dgram")
+const { Socket } = require('dgram')
 
 const DATA = JSON.parse(FS.readFileSync(DATABASE_FILE))
 
+const uuid = () => { let a,b;for(b=a='';b++<36;a+=~b%5|b*3&4?(b^15?8^Math.random()*(b^20?16:4):4).toString(16):'-');return a }
 
 APP.use(EXPRESS.static('Public'))
 
-APP.get('/', (req, res) => res.sendFile("portal.html", { root: __dirname }))
+APP.get('/', (req, res) => res.sendFile('portal.html', { root: __dirname }))
 
 SERVER.listen(PORT, () => {
 
     IO.on('connection', SOCKET => {
 
-        SOCKET.on('user-connected', e =>{
-            SOCKET.emit('init', DATA.games)
+        SOCKET.on('user-connected', e => {
+            SOCKET.emit('init', DATA)
         })
 
         SOCKET.on('disconnect', e => {
-            console.log("user disconnnected")
+            console.log('user disconnnected')
         })
 
         SOCKET.on('pick-game', choice => {
-            
             console.log(choice)
         })
     })
+
     console.log(`listening at http://localhost:${PORT}`)
 })

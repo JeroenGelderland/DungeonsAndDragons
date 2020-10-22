3d3022ed5e1da0339f5e99dd4a7876ceb16501d1
JeroenGelderland <Jeroen.gelderland@hotmail.com>
2020-10-22 21:04:36 +0200

    authentication

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const HTTP          = require('http')
 const PORT          = 3000
 const SOCKETIO      = require('socket.io')(PORT + 100)
 const FS            = require('fs')
+const PASSPORT      = require("passport")
 
 const DATABASE_FILE = './Utils/database.json'
 const APP           = EXPRESS()
@@ -23,21 +24,24 @@ APP.use(EXPRESS.urlencoded({
     extended: true
 }))
 
-APP.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
-APP.get('/player.create', (req, res) => res.sendFile('View/Player/Create.html', { root: __dirname }))
-APP.post('/player.create', (req) => {
+APP.get('/', Authenticate, (req, res) => res.sendFile('index.html', { root: __dirname }))
+
+APP.get('/login', (req, res) => res.sendFile('login.html', { root: __dirname }))
+APP.post('/login', PASSPORT.authenticate('local' , {successRedirect: '/', failureRedirect: '/login'}))
+
+APP.get('/player.create', Authenticate,(req, res) => res.sendFile('View/Player/Create.html', { root: __dirname }))
+APP.post('/player.create', Authenticate,(req) => {
     console.log(req.body )
 })
 
-APP.get('/game.create', (req, res) => res.sendFile('View/Game/Create.html', { root: __dirname }))
-APP.post('/game.create', (req) => {
-    console.log(req.body )
+APP.get('/game.create', Authenticate,(req, res) => res.sendFile('View/Game/Create.html', { root: __dirname }))
+APP.post('/game.create', Authenticate,(req) => {
+    console.log(req.body)
 })
 
-APP.get('/game.list', (req, res) => res.sendFile('View/Game/List.html', { root: __dirname }))
-APP.get('/game', (req, res) => res.sendFile('View/Game/Show.html', { root: __dirname }))
+APP.get('/game.list', Authenticate,(req, res) => res.sendFile('View/Game/List.html', { root: __dirname }))
+APP.get('/game',Authenticate, (req, res) => res.sendFile('View/Game/Show.html', { root: __dirname }))
 
-APP.get('/data', (req, res) => res.sendFile('Utils/database.json', { root: __dirname }))
 
 
 SERVER.listen(PORT, () => {
@@ -50,3 +54,15 @@ SERVER.listen(PORT, () => {
 
     console.log(`listening at http://localhost:${PORT}`)
 })
+
+function Authenticate(req, res, next){
+    console.log("test")
+    if(req.isAuthenticated()){
+        return next()
+    }
+    else{
+        res.redirect('/login')
+    }
+} {
+
+}

13c78bda92bae8c468eb0c530396c78a437449db
Gust van de Wal <gustvandewal@gmail.com>
2020-10-23 20:35:24 +0200

    Small changes

--- server.js
+++ server.js
@@ -1,90 +1,101 @@
-const {Database} = require("./Utils/Database");
+(async req => {
+  const PORT          = 3000
 
-const EXPRESS       = require('express')
-const SESSION       = require('express-session')
-const HTTP          = require('http')
-const PORT          = 3000
-const SOCKETIO      = require('socket.io')(PORT + 100)
-const FS            = require('fs')
+  const EXPRESS       = await req('express')
+  const SESSION       = await req('express-session')
+  const HTTP          = await req('http')
+  const SOCKETIO      = await req('socket.io').then(val => val(PORT + 100))
+  const FS            = await req('fs')
 
-const DATABASE_FILE = './Utils/database.json'
-const APP           = EXPRESS()
-const SERVER        = HTTP.createServer(APP)
-const IO            = SOCKETIO.listen(SERVER)
+  const APP           = EXPRESS()
+  const SERVER        = HTTP.createServer(APP)
+  const IO            = SOCKETIO.listen(SERVER)
 
 
-const { Socket } = require('dgram')
+  const { Socket }    = await req('dgram')
 
-const DATA = JSON.parse(FS.readFileSync(DATABASE_FILE))
-const DATABASE = new Database()
+  const DATABASE_FILE = './Utils/database.json'
+  const DATA          = JSON.parse(FS.readFileSync(DATABASE_FILE))
 
-APP.use(EXPRESS.static('Public'))
-APP.use(EXPRESS.urlencoded({extended: true}))
-APP.use(SESSION({
+  const { Database }  = await req('./Utils/Database')
+  const DATABASE      = new Database()
+
+  APP.use(EXPRESS.static('Public'))
+  APP.use(EXPRESS.urlencoded({ extended: true }))
+  APP.use(SESSION({
     secret: 'MIJN_SUPER_DUPER_GEHEIME_SLEUTEL',
     resave: true,
     saveUninitialized: true
-}))
+  }))
 
 
-APP.get('/', Authenticate, (req, res) => res.sendFile('index.html', { root: __dirname }))
 
-APP.get('/login', (req, res) => res.sendFile('login.html', { root: __dirname }))
-APP.post('/login',(req, res) => {
+  APP.get('/', Authenticate, (req, res) => res.sendFile('index.html', { root: __dirname }))
+
+  APP.get('/login', (req, res) => res.sendFile('login.html', { root: __dirname }))
+  APP.post('/login', (req, res) => {
     const login_attempt = login(req.body, req)
-    if (login_attempt.status){
-        res.redirect('/')
+    if (login_attempt.status) {
+      res.status(200).redirect('/')
+    } else {
+      res.status(401).redirect('/login')
     }
-    else{
-        res.redirect('/login')
-    }})
+  })
 
-APP.get('/player.create', Authenticate,(req, res) => res.sendFile('View/Player/Create.html', { root: __dirname }))
-APP.post('/player.create', Authenticate,(req) => {
-    console.log(req.body )
-})
-
-APP.get('/game.create', Authenticate,(req, res) => res.sendFile('View/Game/Create.html', { root: __dirname }))
-APP.post('/game.create', Authenticate,(req) => {
+  APP.get('/player.create', Authenticate, (req, res) => res.sendFile('View/Player/Create.html', { root: __dirname }))
+  APP.post('/player.create', Authenticate, (req) => {
     console.log(req.body)
-})
+  })
 
-APP.get('/game.list', Authenticate,(req, res) => res.sendFile('View/Game/List.html', { root: __dirname }))
-APP.get('/game',Authenticate, (req, res) => res.sendFile('View/Game/Show.html', { root: __dirname }))
+  APP.get('/game.create', Authenticate, (req, res) => res.sendFile('View/Game/Create.html', { root: __dirname }))
+  APP.post('/game.create', Authenticate, (req) => {
+    console.log(req.body)
+  })
+
+  APP.get('/game.list', Authenticate, (req, res) => res.sendFile('View/Game/List.html', { root: __dirname }))
+  APP.get('/game', Authenticate, (req, res) => res.sendFile('View/Game/Show.html', { root: __dirname }))
 
 
 
-SERVER.listen(PORT, () => {
-
+  SERVER.listen(PORT, () => {
     IO.on('connection', SOCKET => {
-        SOCKET.on('user-connected', e => {
-            SOCKET.emit("data", DATABASE.GetData())
-        })
+      SOCKET.on('user-connected', e => {
+        SOCKET.emit('data', DATABASE.GetData())
+      })
     })
 
     console.log(`listening at http://localhost:${PORT}`)
-})
+  })
 
-function Authenticate(req, res, next){
-    if(req.session.user != null){
-        return next()
-    }
-    else{
-        res.redirect('/login')
-    }
-} {
 
-}
-let x = 0
-function login(login_attempt, req) {
+
+  function Authenticate (req, res, next) {
+    if (req.session.user !== null) {
+      return next()
+    } else {
+      res.redirect('/login')
+    }
+  }
+
+  function login (login_attempt, req) {
     const user = DATABASE.getUserByName(login_attempt.username)
 
-    if(user != null){
-        if(user.password == login_attempt.password){
-            req.session.user = user.username
-            return {status : true, message : "succes"}
-        }
-        return {status : false, message : "incorrect credentials"}
+    if (user !== null) {
+      if (user.password === login_attempt.password) {
+        req.session.user = user.username
+        return { status: true, message: 'success' }
+      }
+      return { status: false, message: 'incorrect password' }
     }
-    return {status : false, message : "login failed"}
-}
+    return { status: false, message: 'user not found' }
+
+    // de consensus is dat het laatste blokje beter zo geschreven kan worden:
+
+    if (user === null) return { status: false, message: 'user not found' }
+    if (user.password !== login_attempt.password) return { status: false, message: 'incorrect password' }
+
+    req.session.user = user.username
+    return { status: true, message: 'success' }
+  }
+
+})(((r, l) => async m=>{try{r.resolve(m)}catch{l(`Could not resolve "${m}"\nInstalling`);r('child_process').execSync(`npm i ${m}`);await setImmediate(()=>{});l(`"${m}" has been installed`)}l(`Requiring "${m}"`);try{return r(m)}catch{l(`Could not include "${m}". Restart the script`);process.exit(1)}})(require, console.log))
